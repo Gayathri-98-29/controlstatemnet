8135a94f488312f0d02d33406b3ac1d017fc7cbe
Gayathri-98-29 <gayathrihbk@gmail.com>
2024-07-23 15:08:55 +0530

    Update Char.java

--- src/assesment/Char.java
+++ src/assesment/Char.java
@@ -4,8 +4,8 @@ public class Char {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char a;
-		for( a ='A';  a <='Z';a++)
+		char a;//declaring the variables
+		for( a ='A';  a <='Z';a++)//to print alphabets
 		{
 			System.out.println(a);
 		}
