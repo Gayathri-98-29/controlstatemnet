08fa4d6fbae63a8e011b5e55bf71faa7f940bf97
Gayathri-98-29 <gayathrihbk@gmail.com>
2024-07-23 15:15:02 +0530

    Update Greatestoffour.java

--- src/assesment/Greatestoffour.java
+++ src/assesment/Greatestoffour.java
@@ -4,14 +4,14 @@ public class Greatestoffour {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=900,b=200,c=300,d=400;
-if(a+b>c+d)
+int a=900,b=200,c=300,d=400;//declaring variables
+if(a+b>c+d)//condition checking
 {
-	System.out.println("the sum of a and b is greater than sum of c and d");
+	System.out.println("the sum of a and b is greater than sum of c and d");//true
 	}
 else
 {
-	System.out.println("the sum of a and b is less than sum of c and d");
+	System.out.println("the sum of a and b is less than sum of c and d");//false
 }
 }
 }
